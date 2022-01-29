716fca2922bc916c52f14a25cbe45418bab8bc0a
mustafanooristani <mm.nooristani1@gmail.com>
2022-01-29 11:53:30 -0500

    Feature Sun with errors to be resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,10 @@ public class Main {
         milkyWay.sun=1;
         milkyWay.moon=2;
         milkyWay.planets=3;
-        
+
+        SUN Solana=new SUN();
+        Solana.Heat="4354363636kj";
+        Solana.radius="3425346363km";
+        Solana.temp=2144235235c;
     }
 }
